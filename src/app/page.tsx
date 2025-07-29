eb43031f75ae5491fa20127047d1a98b463d7faa
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-07-29 19:10:49 +0300

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-
 export default function Home() {
   return (
-    <></>
+    <div className="absolute left-[50%] translate-x-[-50%] translate-y-[-50%] top-[40%] flex flex-col">
+      <p className="text-center text-3xl font-fantasy text-white">Weberos</p>
+      <p className="text-center text-xl font-heading text-gray-light">
+        Next.js Template
+      </p>
+    </div>
   );
 }
