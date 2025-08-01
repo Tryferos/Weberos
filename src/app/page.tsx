1f592bd4983c3db6de1d9793d95e8d7ca79ada72
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-08-01 12:34:56 +0300

    add homepagescreen component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-export default function Home() {
-  return (
-    <div className="absolute left-[50%] translate-x-[-50%] translate-y-[-50%] top-[40%] flex flex-col">
-      <p className="text-center text-3xl font-fantasy text-white">Weberos</p>
-      <p className="text-center text-xl font-heading text-gray-light">
-        Next.js Template
-      </p>
-    </div>
-  );
+import {HomePageScreen} from '@components/HomePage/HomePageScreen';
+import {UserNetwork} from '@network/user-network';
+
+export default async function Home() {
+  return <HomePageScreen />;
 }
---
+++ src/components/HomePage/HomePageScreen.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import {useWeberosQuery} from '@hooks/useWeberosQuery';
+import Network from '@network/index';
+
+export const HomePageScreen = () => {
+  return (
+    <div className="absolute left-[50%] translate-x-[-50%] gap-y-1 translate-y-[-50%] top-[40%] flex flex-col">
+      <p className="text-center text-3xl font-fantasy text-white">Weberos</p>
+      <p className="text-center text-xl font-heading text-gray-light">
+        Next.js Template
+      </p>
+    </div>
+  );
+};
