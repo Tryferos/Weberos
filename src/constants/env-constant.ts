79df004b63ff4e02edd0dd0d46bda246cf3a7ce2
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-08-02 12:14:05 +0300

    add socket url to env

--- src/constants/env-constant.ts
+++ src/constants/env-constant.ts
@@ -4,5 +4,6 @@ export const Environment: EnvSchemaType = z.parse(
   EnvSchema,
   Object.freeze({
     API_URL: process.env.NEXT_PUBLIC_API_URL,
+    SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
   }),
 );
