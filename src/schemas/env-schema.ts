8f123f762aebd95a8b6540e545bc7f2ef5e42dd3
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-08-02 12:14:23 +0300

    update env schema to add socket url and match protocols with regex

--- src/schemas/env-schema.ts
+++ src/schemas/env-schema.ts
@@ -1,7 +1,8 @@
 import {z} from 'zod';
 
 const EnvSchema = z.object({
-  API_URL: z.url(),
+  API_URL: z.url({protocol: /^http[s]?/}),
+  SOCKET_URL: z.url({protocol: /^ws[s]?/}),
 });
 
 export type EnvSchemaType = z.infer<typeof EnvSchema>;
