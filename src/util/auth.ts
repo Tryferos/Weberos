ead2393128d2aacdae22115807de21e9f76b09a4
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-08-04 11:25:53 +0300

    add try catch for auth

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -3,9 +3,14 @@ import {UserModel} from '@models/user';
 import {getServerSession} from 'next-auth/next';
 
 export const getCurrentUser = async () => {
-  const session = await getServerSession();
-  if (session && session.user) {
-    return await UserModel.findOne({email: session.user.email}).lean();
+  try {
+    const session = await getServerSession();
+    if (session && session.user) {
+      return await UserModel.findOne({email: session.user.email}).lean();
+    }
+    return null;
+  } catch (err) {
+    console.log(err);
+    return null;
   }
-  return null;
 };
