8207ffd7608e1047aa0f4affe6257c7af81b3824
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-08-04 11:25:04 +0300

    add grid mask

--- src/components/Screens/HomeScreen/HomePageScreen.tsx
+++ src/components/Screens/HomeScreen/HomePageScreen.tsx
@@ -1,19 +1,21 @@
 'use client';
 import {WeberosIcon} from '@components/atoms/WeberosIcon';
-import {useWeberosQuery} from '@hooks/useWeberosQuery';
-import Network from '@network/index';
+import GridMask from '@components/masks/GridMask';
 import {useSession, signIn, signOut} from 'next-auth/react';
 
 export const HomePageScreen = () => {
   return (
-    <div className="absolute left-[50%] translate-x-[-50%] gap-y-1 translate-y-[-50%] top-[40%] flex flex-col">
-      <p className="text-center text-3xl font-fantasy text-white">Weberos</p>
-      <p className="text-center text-xl font-heading text-gray-light">
-        Next.js Template
-      </p>
-      <SignedInComponent />
-      <SignedOutComponent />
-    </div>
+    <>
+      <div className="absolute left-[50%] translate-x-[-50%] gap-y-1 translate-y-[-50%] top-[40%] flex flex-col">
+        <p className="text-center text-3xl font-fantasy text-white">Weberos</p>
+        <p className="text-center text-xl font-heading text-gray-light">
+          Next.js Template
+        </p>
+        <SignedInComponent />
+        <SignedOutComponent />
+      </div>
+      <GridMask />
+    </>
   );
 };
 
--- src/components/masks/GridMask.tsx
+++ src/components/masks/GridMask.tsx
@@ -3,9 +3,9 @@ const GridMask = () => {
   return (
     <div
       className="h-full w-full pointer-events-none fixed inset-0 
-             bg-[linear-gradient(to_right,rgba(0,0,0,0.04)_1px,transparent_1px),linear-gradient(to_bottom,rgba(0,0,0,0.04)_1px,transparent_1px)]
+             bg-[linear-gradient(to_right,rgba(214,211,209,0.04)_1px,transparent_1px),linear-gradient(to_bottom,rgba(214,211,209,0.04)_1px,transparent_1px)]
              bg-[size:96px_96px]
-             mask-radial-at-center mask-radial-from-0% mask-radial-to-100% 
+             mask-radial-at-[50%_75%] mask-radial-from-0% mask-radial-to-50% 
              [mask-mode:match-source]
              [mask-repeat:no-repeat]"></div>
   );
