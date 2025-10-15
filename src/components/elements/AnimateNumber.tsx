10a23c3661597f12d0324861483d96504dddcea9
Tryferos <truf022@gmail.com>
2025-10-15 11:19:41 +0300

    adjust animate number to provide ease out functionality

--- src/components/elements/AnimateNumber.tsx
+++ src/components/elements/AnimateNumber.tsx
@@ -9,38 +9,49 @@ type Props = {
 export default function AnimateNumber({
   value,
   className,
-  duration = 20,
+  duration = 100,
 }: Props) {
   const elementRef = useRef<HTMLHeadingElement>(null);
   const isVisible = useInView(elementRef);
 
-  const invervalRef = useRef<NodeJS.Timeout | null>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
   const [showingNumber, setShowingNumber] = useState(0);
   const showingNumberRef = useRef(showingNumber);
 
+  const easeOut = (number: number) => {
+    const progress = number / value;
+    const normalizedProgress = progress * 2 - 1;
+    const easedProgress = Math.max(0, Math.pow(normalizedProgress, 3) * 1.5);
+    return easedProgress * duration;
+  };
+
   useEffect(() => {
+    showingNumberRef.current = 0;
+    setShowingNumber(0);
+    const tick = () => {
+      if (showingNumberRef.current >= value) {
+        return;
+      }
+      showingNumberRef.current = showingNumberRef.current + 1;
+      setShowingNumber(showingNumberRef.current);
+
+      timeoutRef.current = setTimeout(tick, easeOut(showingNumberRef.current));
+    };
     if (isVisible) {
-      invervalRef.current = setInterval(() => {
-        if (showingNumberRef.current >= value) {
-          clearInterval(invervalRef.current!);
-          return;
-        }
-        showingNumberRef.current = showingNumberRef.current + 1;
-        setShowingNumber(prev => prev + 1);
-      }, duration);
-    } else if (invervalRef.current) {
-      clearInterval(invervalRef.current);
+      tick();
     }
     return () => {
-      clearInterval(invervalRef.current!);
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
     };
   }, [value, duration, isVisible]);
 
   return (
-    <h2
+    <span
       ref={elementRef}
       className={cn('text-white font-heading text-3xl', className)}>
       {showingNumber}
-    </h2>
+    </span>
   );
 }
