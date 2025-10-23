7d6636469b32f451c97c70022a8de2e604b85116
Tryferos <truf022@gmail.com>
2025-10-23 17:44:21 +0300

    feat - adjust scroll view

--- src/components/elements/ScollView.tsx
+++ src/components/elements/ScollView.tsx
@@ -1,17 +1,32 @@
-import {PropsWithChildren} from 'react';
-import cn from '../../util/cn';
+'use client';
+import {ComponentPropsWithRef, useMemo} from 'react';
+import cn from '@util/cn';
 
 type Props = {
   className?: string;
-} & PropsWithChildren &
-  React.ComponentPropsWithRef<'div'>;
+  direction?: 'horizontal' | 'vertical';
+} & ComponentPropsWithRef<'div'>;
 
-export default function ScrollView({className, children, ref}: Props) {
-  return (
-    <div
-      ref={ref}
-      className={cn('overflow-y-auto h-full custom-scrollbar', className)}>
-      {children}
-    </div>
-  );
+export default function ScrollView(props: Props) {
+  const {className, direction, children, id, ...rest} = props;
+
+  const ScrollViewComponent = useMemo(() => {
+    return (
+      <div
+        {...rest}
+        id={id}
+        className={cn(
+          'custom-scrollbar',
+          direction === 'horizontal'
+            ? 'overflow-x-auto w-full horizontal'
+            : 'overflow-y-auto h-full',
+
+          className,
+        )}>
+        {children}
+      </div>
+    );
+  }, [id, direction, className, children]);
+
+  return ScrollViewComponent;
 }
