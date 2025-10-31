690b27ccbae3430c9856948d0ac613009509c028
Tryferos <truf022@gmail.com>
2025-10-31 16:56:27 +0200

    wrap weberos icon with suspense

--- src/components/atoms/WeberosIcon.tsx
+++ src/components/atoms/WeberosIcon.tsx
@@ -1,7 +1,14 @@
-import {ComponentType, ReactNode, useMemo} from 'react';
+import {ComponentType, Suspense, useMemo} from 'react';
 import cn from '../../util/cn';
 import dynamic from 'next/dynamic';
-export type WeberosIconNames = 'cancel' | 'cancel-circle' | 'google' | 'search';
+export type WeberosIconNames =
+  | 'cancel'
+  | 'cancel-circle'
+  | 'google'
+  | 'search'
+  | 'react'
+  | 'golang'
+  | 'javascript';
 type WeberosIconProps = {
   name: WeberosIconNames;
   className?: string;
@@ -16,6 +23,16 @@ export const WeberosIcon = ({
   containerClassName,
 }: WeberosIconProps) => {
   const Icon = useMemo(() => Icons[name], [name]);
+  const SuspenseSkeleton = useMemo(() => {
+    return (
+      <figure
+        aria-hidden="true"
+        className={cn(
+          'size-6 rounded-full bg-gray-200/50 animate-pulse',
+          className,
+        )}></figure>
+    );
+  }, [className]);
   if (onClick) {
     return (
       <div
@@ -24,11 +41,17 @@ export const WeberosIcon = ({
           'p-2 cursor-pointer group hover:bg-icon-hover rounded-full self-start flex-none',
           containerClassName,
         )}>
-        <Icon className={className} />
+        <Suspense fallback={SuspenseSkeleton}>
+          <Icon className={className} />
+        </Suspense>
       </div>
     );
   } else if (Icon) {
-    return <Icon className={className} />;
+    return (
+      <Suspense fallback={SuspenseSkeleton}>
+        <Icon className={className} />
+      </Suspense>
+    );
   } else {
     return null;
   }
@@ -44,4 +67,7 @@ const Icons: {
   'cancel-circle': dynamic(() => import('@icons/svg/icon-cancel-circle')),
   google: dynamic(() => import('@icons/svg/icon-google')),
   search: dynamic(() => import('@icons/svg/icon-search')),
+  react: dynamic(() => import('@icons/svg/icon-react')),
+  javascript: dynamic(() => import('@icons/svg/icon-javascript')),
+  golang: dynamic(() => import('@icons/svg/icon-golang')),
 };
